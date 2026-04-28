65b5f2d6383f3c3a669085d9c750f71f9bcd0e0b
cheikhjad10 <cheikhjad10@gmail.com>
2026-04-28 22:56:02 +0300

    finalized firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:studyplanner/firebase_options.dart';
 import 'package:studyplanner/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 

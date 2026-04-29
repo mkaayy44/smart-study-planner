2c17c47fb2fc29407222aeac5c666de694ce2f62
cheikhjad10 <cheikhjad10@gmail.com>
2026-04-29 23:50:34 +0300

    profile fetch correct info of user

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,33 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:studyplanner/app%20design/app_card.dart';
-import 'package:studyplanner/app%20design/app_colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:studyplanner/app design/app_card.dart';
+import 'package:studyplanner/app design/app_colors.dart';
+import 'package:studyplanner/screens/login_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
+    if (user == null) {
+      return Scaffold(
+        body: Center(child: Text("No user logged in")),
+      );
+    }
+
     return Scaffold(
       backgroundColor: AppColors.bg,
       body: Padding(
         padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            SizedBox(height: 40),
+        child: FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance
+              .collection('users')
+              .doc(user!.uid)
+              .get(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-            CircleAvatar(radius: 40),
-            SizedBox(height: 10),
+            final data = snapshot.data!.data() as Map<String, dynamic>;
 
-            Text("Your Name", style: AppStyles.title),
-            Text("your@email.com", style: AppStyles.subtitle),
+            final name = data['name'] ?? "No Name";
+            final email = user!.email ?? "No Email";
+            final phoneNumber = data['phoneNumber'] ?? "";
 
-            SizedBox(height: 30),
+            return Column(
+              children: [
+                SizedBox(height: 40),
 
-            AppCard(
-              child: ListTile(
-                title: Text("Logout"),
-                trailing: Icon(Icons.logout),
-              ),
-            )
-          ],
+                CircleAvatar(radius: 40),
+                SizedBox(height: 10),
+
+                Text(name, style: AppStyles.title),
+                Text(email, style: AppStyles.subtitle),
+                Text(phoneNumber, style: AppStyles.subtitle),
+                
+
+                SizedBox(height: 30),
+
+                AppCard(
+                  child: ListTile(
+                    title: Text("Logout"),
+                    trailing: Icon(Icons.logout),
+                    onTap: () async {
+                      await FirebaseAuth.instance.signOut();
+
+                      if (!context.mounted) return;
+
+                      Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                          builder: (_) => LoginScreen(),
+                        ),
+                        (route) => false,
+                      );
+                    },
+                  ),
+                )
+              ],
+            );
+          },
         ),
       ),
     );
--- lib/todo.md
+++ lib/todo.md
@@ -3,14 +3,27 @@
     - add correct fields for tasks addition not only title DONE
     - remove light pink bg in bottom navbar DONE
     - make tasks all can add not only logged in DONE
+    - profile fetch correct info of user DONE
+    - make logout working
+    - if swipe to complete task make option to see completed tasks
+    
+    DESIGN TASKS
     - make add button more aesthetic
     - make focus screen more aesthetic
-    - profile fetch correct info of user
     - add images in signup and login screens
     - fix skip in signup screen not so down
     - see if we can add header
-    - make logout working
     - smooth nav between pages i want
     - change outside icon app
     - make ui better for all pages but till now good
-    - if swipe to complete task make option to see completed tasks
+
+    TECHNICAL TASKS
+    - task reminder if deadline near notification
+    - study schedule generator
+    - urgency score calculator
+    - 'im overwhelmed' button keeps only imp stuff
+    - smart scheduling filters in tasks page (sort tasks by deadline, priority, diffc, assign to days)
+    - automatically suggests when to study task
+    - pomodoro focus save data session backend if logged in 
+    - productivity analysis (hours/day, completed tasks, streak, charts)
+    - home page analytics more
